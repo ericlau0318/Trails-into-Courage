b03d264adae979fea1ad87d6793db1b60ffb4eb0
Briankam251 <69844252+Briankam251@users.noreply.github.com>
2024-05-01 03:34:02 +0800

    123

--- TrailsintoCourage(URP)/Assets/Script/System/ShopPanelController.cs
+++ TrailsintoCourage(URP)/Assets/Script/System/ShopPanelController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ShopPanelController : MonoBehaviour
 {
     public GameObject ThunderSkillsText;
@@ -9,6 +9,10 @@ public class ShopPanelController : MonoBehaviour
     public GameObject UsingBtn1;
     public GameObject UsingBtn2;
     public GameObject UsingBtn3;
+    public Text UsingBtn1Text;
+    public Text UsingBtn2Text;
+    public Text UsingBtn3Text;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +26,23 @@ public class ShopPanelController : MonoBehaviour
             ThunderSkillsText.SetActive(false);
             UsingBtn3.SetActive(true);
         }
+    }
+    public void IsUseBtn1()
+    {
+        UsingBtn1Text.text = "Is Use";
+        UsingBtn2Text.text = "Not Use";
+        UsingBtn3Text.text = "Not Use";
+    }
+    public void IsUseBtn2()
+    {
+        UsingBtn1Text.text = "Not Use";
+        UsingBtn2Text.text = "Is Use";
+        UsingBtn3Text.text = "Not Use";
+    }
+    public void IsUseBtn3()
+    {
+        UsingBtn1Text.text = "Not Use";
+        UsingBtn2Text.text = "Not Use";
+        UsingBtn3Text.text = "Is Use";
     }
 }
