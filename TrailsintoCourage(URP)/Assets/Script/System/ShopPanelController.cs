86e0103953f8ba3151fa959e23be0b4c89835e17
Briankam251 <69844252+Briankam251@users.noreply.github.com>
2024-05-01 03:52:06 +0800

    123

--- TrailsintoCourage(URP)/Assets/Script/System/ShopPanelController.cs
+++ TrailsintoCourage(URP)/Assets/Script/System/ShopPanelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 public class ShopPanelController : MonoBehaviour
 {
@@ -12,10 +13,25 @@ public class ShopPanelController : MonoBehaviour
     public Text UsingBtn1Text;
     public Text UsingBtn2Text;
     public Text UsingBtn3Text;
+    public static bool skillOneUsing;
+    public static bool skillTwoUsing;
+    public static bool skillThreeUsing;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (skillOneUsing == true)
+        {
+            IsUseBtn1();
+        }
+        if (skillTwoUsing == true)
+        {
+            IsUseBtn2();
+        }
+        if (skillThreeUsing == true)
+        {
+            IsUseBtn3();
+        }
         if (Level1GameManager.IsLevel1Pass == true)
         {
             IceSkillsText.SetActive(false);
@@ -29,18 +45,27 @@ public class ShopPanelController : MonoBehaviour
     }
     public void IsUseBtn1()
     {
+        skillOneUsing = true;
+        skillTwoUsing = false;
+        skillThreeUsing = false;
         UsingBtn1Text.text = "Is Use";
         UsingBtn2Text.text = "Not Use";
         UsingBtn3Text.text = "Not Use";
     }
     public void IsUseBtn2()
     {
+        skillOneUsing = false;
+        skillTwoUsing = true;
+        skillThreeUsing = false;
         UsingBtn1Text.text = "Not Use";
         UsingBtn2Text.text = "Is Use";
         UsingBtn3Text.text = "Not Use";
     }
     public void IsUseBtn3()
     {
+        skillOneUsing = false;
+        skillTwoUsing = false;
+        skillThreeUsing = true;
         UsingBtn1Text.text = "Not Use";
         UsingBtn2Text.text = "Not Use";
         UsingBtn3Text.text = "Is Use";
